93e389e4853fd55442a24aedb9574e4b7be96de3
Uday Bhasker Dusharla <udaybhasker.dusharla@proquest.com>
2021-12-21 21:53:16 -0500

    modified cst_visitor.js

--- packages/chevrotain/src/parse/cst/cst_visitor.ts
+++ packages/chevrotain/src/parse/cst/cst_visitor.ts
@@ -11,31 +11,92 @@ import includes from "lodash/includes"
 import { defineNameProp } from "../../lang/lang_extensions"
 import { CstNode, ICstVisitor } from "@chevrotain/types"
 
-export function defaultVisit<IN>(ctx: any, param: IN): void {
-  const childrenNames = keys(ctx)
-  const childrenNamesLength = childrenNames.length
+export function defaultVisit<IN>(ctx: any, param: any, matchingIndices?: any): void {
+  const childrenNames = keys(ctx);
+  const childrenNamesLength = childrenNames.length;
   for (let i = 0; i < childrenNamesLength; i++) {
-    const currChildName = childrenNames[i]
-    const currChildArray = ctx[currChildName]
-    const currChildArrayLength = currChildArray.length
-    for (let j = 0; j < currChildArrayLength; j++) {
-      const currChild: any = currChildArray[j]
+    const currChildName = childrenNames[i];
+    if (currChildName === 'name') return;
+    const currChildArray = ctx[currChildName];
+    const currChildArrayLength = currChildArray.length;
+
+    for (var j = 0; j < currChildArrayLength; j++) {
+      const currChild = currChildArray[j];
       // distinction between Tokens Children and CstNode children
       if (currChild.tokenTypeIdx === undefined) {
-        this[currChild.name](currChild.children, param)
+        if (currChild.fullName !== undefined) {
+          param = appendPathToParam(currChild.fullName, param);
+          this[currChild.fullName](currChild.children, param);
+        }
+        else {
+          let name = currChild.name;
+          let hasIndex = false;
+          let indexMap = getMatchingIndexMap(currChild.name, matchingIndices || (param && param.matchingIndices));
+          if (indexMap) {
+            name = indexMap['name'] + '~' + indexMap['index'];
+            if (j != indexMap['index']) {
+              if (j > 0 && currChildArrayLength > 1) {
+                continue;
+              }
+            }
+            hasIndex = true;
+          } else if (this && this.collectorName) {
+            let parts = this.collectorName.split('~');
+            if (parts.length > 1 && parts[0] === currChild.name) {
+              name = this.collectorName;
+
+              if (parts[1] != j) {
+                if (j > 0 && currChildArrayLength > 1) {
+                  continue;
+                }
+              }
+              hasIndex = true;
+            }
+          }
+
+          param = appendPathToParam(name, param);
+          if (hasIndex && !this[name]) {
+            name = currChild.name;
+          }
+          this[name](currChild.children, param);
+        }
+      } else {
+        if (this[currChild.tokenType.name]) {
+          param = appendPathToParam(currChild.tokenType.name, param);
+          this[currChild.tokenType.name](currChild, param);
+          if (param && param.path) param.path = '';
+        }
       }
     }
   }
   // defaultVisit does not support generic out param
+  return undefined;
 }
-
+function getMatchingIndexMap(step: any, matchingIndices: any) {
+  let retIndexMap;
+  matchingIndices && matchingIndices.map((indexMap: any) => {
+    let parts = indexMap.name.split('~');
+    if (parts[0] === step) {
+      retIndexMap = indexMap;
+      return;
+    }
+  });
+  return retIndexMap;
+}
+function appendPathToParam(step: any, param: any) {
+  if (!step) return;
+  if (!param) param = {};
+  if (!param.path) param.path = '';
+  param.path = (param.path ? param.path + '>' : '') + step;
+  return param;
+}
 export function createBaseSemanticVisitorConstructor(
   grammarName: string,
   ruleNames: string[]
 ): {
-  new (...args: any[]): ICstVisitor<any, any>
+  new(...args: any[]): ICstVisitor<any, any>
 } {
-  const derivedConstructor: any = function () {}
+  const derivedConstructor: any = function () { }
 
   // can be overwritten according to:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/
@@ -43,20 +104,36 @@ export function createBaseSemanticVisitorConstructor(
   defineNameProp(derivedConstructor, grammarName + "BaseSemantics")
 
   const semanticProto = {
-    visit: function (cstNode: CstNode | CstNode[], param: any) {
+    visit: function (cstNode: any | any[], param: any) {
       // enables writing more concise visitor methods when CstNode has only a single child
       if (isArray(cstNode)) {
         // A CST Node's children dictionary can never have empty arrays as values
         // If a key is defined there will be at least one element in the corresponding value array.
-        cstNode = cstNode[0]
+        cstNode = cstNode[0];
       }
-
       // enables passing optional CstNodes concisely.
       if (isUndefined(cstNode)) {
-        return undefined
+        return undefined;
       }
-
-      return this[cstNode.name](cstNode.children, param)
+      if (cstNode.fullName !== undefined) {
+        param = appendPathToParam(cstNode.fullName, param);
+        return this[cstNode.fullName](cstNode.children, param);
+      }
+      else {
+        if (cstNode.tokenTypeIdx) {
+          param = appendPathToParam(cstNode.tokenType.name, param);
+          return this[cstNode.tokenType.name](cstNode, param);
+        }
+        if (param && param.matchingIndices && param.matchingIndices.length) {
+          return defaultVisit.call(this, cstNode.children, param, param.matchingIndices);
+        } else {
+          let parts = cstNode.name.split('~');
+          if (parts.length > 1) {
+            return defaultVisit.call(this, cstNode.children, param, [{ name: parts[0], index: parts[1] }]);
+          }
+        }
+        return this[cstNode.name](cstNode.children, param, param && param.matchingIndices);
+      }
     },
 
     validateVisitor: function () {
@@ -68,7 +145,7 @@ export function createBaseSemanticVisitorConstructor(
         )
         throw Error(
           `Errors Detected in CST Visitor <${this.constructor.name}>:\n\t` +
-            `${errorMessages.join("\n\n").replace(/\n/g, "\n\t")}`
+          `${errorMessages.join("\n\n").replace(/\n/g, "\n\t")}`
         )
       }
     }
@@ -87,9 +164,9 @@ export function createBaseVisitorConstructorWithDefaults(
   ruleNames: string[],
   baseConstructor: Function
 ): {
-  new (...args: any[]): ICstVisitor<any, any>
+  new(...args: any[]): ICstVisitor<any, any>
 } {
-  const derivedConstructor: any = function () {}
+  const derivedConstructor: any = function () { }
 
   // can be overwritten according to:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/
